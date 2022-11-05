07670f1b1d558b25299466a203b4c7a051ec5ace
bizu-collab <bizualemtilaye@gmail.com>
2022-11-05 02:26:06 -0700

    try container

--- app.js
+++ app.js
@@ -1,3 +1,13 @@
-document.addEventListener('DOMContentLoaded',() =>{
+const cvs=document.getElementById("tetries"); 
+const ctx=cvs.getContext("2d");
+const ROW=20;
+const COL=COLUMN=10;
+const SQ=squareSize=20;
+const  VACANT="WHITE";
 
-})
+ctx.fillStyle= "red";
+
+ctx.fillRect(0,0,50,50);
+
+ctx.fillStyle="BLACK";
+ctx.strokeRect(0,0,50,50);
