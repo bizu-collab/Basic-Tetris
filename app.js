9852ac1591369b8b68c8be0f1cac887fe63ee82f
bizu-collab <bizualemtilaye@gmail.com>
2022-11-06 10:26:38 -0800

    update

--- app.js
+++ app.js
@@ -5,9 +5,28 @@ const COL=COLUMN=10;
 const SQ=squareSize=20;
 const  VACANT="WHITE";
 
-ctx.fillStyle= "red";
+function drawSquare(x,y,color){
+    ctx.fillStyle = color;
+    ctx.fillRect(x*SQ,y*SQ,SQ,SQ);
 
-ctx.fillRect(0,0,50,50);
+    ctx.strokeStyle = "BLACK";
+    ctx.strokeRect(x*SQ,y*SQ,SQ,SQ);
+}
+let board = [];
+for( r = 0; r <ROW; r++){
+    board[r] = [];
+    for(c = 0; c < COL; c++){
+        board[r][c] = VACANT;
+    }
+}
 
-ctx.fillStyle="BLACK";
-ctx.strokeRect(0,0,50,50);
+// draw the board
+function drawBoard(){
+    for( r = 0; r <ROW; r++){
+        for(c = 0; c < COL; c++){
+            drawSquare(c,r,board[r][c]);
+        }
+    }
+}
+
+drawBoard();
